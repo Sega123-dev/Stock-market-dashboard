d24d1c0786f825dc2ff4e58418500290d1ac5920
Sega <brajicsergej@gmail.com>
2025-06-14 13:53:29 +0200

    Grid layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,8 @@
-import { useState } from 'react'
 import './App.css'
+import Dashboard from './components/dashboard'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card bg-red-500 font-quicksand border-1 font-normal f">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <Dashboard/>
 }
 
 export default App
