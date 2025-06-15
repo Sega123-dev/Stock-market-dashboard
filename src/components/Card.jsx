3d86d8b2fd1674610090ac8158e1643bb76e28b5
Sega <brajicsergej@gmail.com>
2025-06-15 22:58:50 +0200

    Header component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Card = ({children}) => {
     return <div className="h-full w-full rounded-md relative p-8 border-2 bg-gray-300">{children}</div>
 }
---
+++ src/components/Search.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react"
+import { mockSearchResults } from "../constants/mock"
+import {XIcon,SearchIcon} from "@heroicons/react/solid"
+const Search = () => {
+    const [input,setInput] = useState("")
+    const [bestMatches,setBestMatches] = useState(mockSearchResults.result)
+
+    const clear = () => {
+        setInput("");
+        setBestMatches([])
+    }
+
+    const updateBestMatches = () => {
+        setBestMatches(mockSearchResults.result)
+    }
+    
+    return <div className="flex items-center my-4 border-2 rounded-md relative z-50 w-96 bg-white border-neutral-200">
+        <input 
+        type="text"
+        value={input}
+        className="w-full px-4 py-2 focus:outline-none rounded-md"
+        placeholder="Search stock..."
+        onChange={(event) => setInput(event.target.value)} 
+        onKeyUp={(event) => {
+            if(event.key === "Enter") {
+                updateBestMatches
+            }
+        }}/>
+
+      {input && (
+        <button onClick={clear} className="m-1">
+          <XIcon className="h-4 w-4 fill-gray-500 cursor-pointer" />
+        </button>
+      )}
+       <button
+        onClick={updateBestMatches}
+        className="h-8 w-8 bg-indigo-600 rounded-md flex justify-center items-center m-1 p-2 cursor-pointer"
+      >
+        <SearchIcon className="h-4 w-4 fill-gray-100" />
+      </button>
+    </div>
+}
+
+export default Search
