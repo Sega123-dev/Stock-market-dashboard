06451348f4f7eb223f575f68971a6cb4e7d2c599
Sega <brajicsergej@gmail.com>
2025-06-17 00:26:58 +0200

    Search dark theme

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,41 +1,53 @@
-import { useState } from "react"
-import { mockSearchResults } from "../constants/mock"
-import {XIcon,SearchIcon} from "@heroicons/react/solid"
+import { XIcon, SearchIcon } from "@heroicons/react/solid";
+import { useContext, useState } from "react";
+import { mockSearchResults } from "../constants/mock";
+import ThemeContext from "../context/ThemeContext";
 import SearchResults from "./SearchResults";
+
 const Search = () => {
-    const [input,setInput] = useState("")
-    const [bestMatches,setBestMatches] = useState(mockSearchResults.result)
+  const { darkMode } = useContext(ThemeContext);
 
-    const clear = () => {
-        setInput("");
-        setBestMatches([])
-    }
+  const [input, setInput] = useState("");
 
-    const updateBestMatches = () => {
-        setBestMatches(mockSearchResults.result)
-    }
-    
-    return <div className="flex items-center my-4 border-2 rounded-md relative z-50 w-96 bg-white border-neutral-200">
-        <input 
+  const [bestMatches, setBestMatches] = useState(mockSearchResults.result);
+
+  const clear = () => {
+    setInput("");
+    setBestMatches([]);
+  };
+
+  const updateBestMatches = () => {
+    setBestMatches(mockSearchResults.result);
+  };
+
+  return (
+    <div
+      className={`flex items-center my-4 border-2 rounded-md relative z-50 w-96 ${
+        darkMode ? "bg-gray-900 border-gray-800" : "bg-white border-neutral-200"
+      }`}
+    >
+      <input
         type="text"
         value={input}
-        className="w-full px-4 py-2 focus:outline-none rounded-md"
+        className={`w-full px-4 py-2 focus:outline-none rounded-md ${
+          darkMode ? "bg-gray-900" : null
+        }`}
         placeholder="Search stock..."
-        onChange={(event) => setInput(event.target.value)} 
-        onKeyUp={(event) => {
-            if(event.key === "Enter") {
-                updateBestMatches
-            }
-        }}/>
-
+        onChange={(event) => setInput(event.target.value)}
+        onKeyPress={(event) => {
+          if (event.key === "Enter") {
+            updateBestMatches();
+          }
+        }}
+      />
       {input && (
         <button onClick={clear} className="m-1">
-          <XIcon className="h-4 w-4 fill-gray-500 cursor-pointer" />
+          <XIcon className="h-4 w-4 fill-gray-500" />
         </button>
       )}
-       <button
+      <button
         onClick={updateBestMatches}
-        className="h-8 w-8 bg-indigo-600 rounded-md flex justify-center items-center m-1 p-2 cursor-pointer"
+        className="h-8 w-8 bg-indigo-600 rounded-md flex justify-center items-center m-1 p-2"
       >
         <SearchIcon className="h-4 w-4 fill-gray-100" />
       </button>
@@ -43,6 +55,7 @@ const Search = () => {
         <SearchResults results={bestMatches} />
       ) : null}
     </div>
-}
+  );
+};
 
-export default Search
+export default Search;
