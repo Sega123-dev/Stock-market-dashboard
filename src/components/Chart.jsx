da83aeee4316449f0c5632843867bdaedaa4c22b
Sega <brajicsergej@gmail.com>
2025-06-17 00:17:49 +0200

    Chart dark theme

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,27 +1,38 @@
-import { useState } from 'react'
-import { mockHistoricalData } from '../constants/mock'
-import { convertUnixTimestampToDate } from '../helpers/date-helper';
-import Card from './Card';
-import ChartFilter from "./ChartFilter";
-import { Area, AreaChart, ResponsiveContainer, Tooltip, XAxis,YAxis } from 'recharts';
+import React, { useContext, useState } from "react";
+import { mockHistoricalData } from "../constants/mock";
+import { convertUnixTimestampToDate } from "../helpers/date-helper";
+import {
+  Area,
+  XAxis,
+  YAxis,
+  ResponsiveContainer,
+  AreaChart,
+  Tooltip,
+} from "recharts";
+import Card from "./Card";
 import { chartConfig } from "../constants/config";
+import ChartFilter from "./ChartFilter";
+import ThemeContext from "../context/ThemeContext";
 
 const Chart = () => {
-  const [data,setData] = useState(mockHistoricalData);
-  const [filter,setFilter] = useState("1W");
+  const [data, setData] = useState(mockHistoricalData);
 
-  const formatData = () => {
-    return data.c.map((item,index) => {
-        return {
-            value: item.toFixed(2),
-            date: convertUnixTimestampToDate(data.t[index])
-        }
-    })
-  }
+  const [filter, setFilter] = useState("1W");
+
+  const { darkMode } = useContext(ThemeContext);
+
+  const formatData = (data) => {
+    return data.c.map((item, index) => {
+      return {
+        value: item.toFixed(2),
+        date: convertUnixTimestampToDate(data.t[index]),
+      };
+    });
+  };
 
   return (
     <Card>
-    <ul className="flex absolute top-2 right-2 z-40">
+      <ul className="flex absolute top-2 right-2 z-40">
         {Object.keys(chartConfig).map((item) => (
           <li key={item}>
             <ChartFilter
@@ -34,22 +45,40 @@ const Chart = () => {
           </li>
         ))}
       </ul>
-        <ResponsiveContainer>
-            <AreaChart data={formatData()}>
-        <defs>
-              <linearGradient id="chartColor" x1="0" y1="0" x2="0" y2="1">
-                 <stop offset="5%" stopColor="rgb(199 210 254)" stopOpacity={0.8}/>
-                 <stop offset="95%" stopColor="rgb(199 210 254)" stopOpacity={0}/>
-              </linearGradient>
-        </defs>
-               <Area type="monotone" dataKey="value" stroke='#312e81' fillOpacity={1} strokeWidth={0.5} fill='url(#chartColor)'/>
-               <Tooltip/>
-               <XAxis dataKey={"date"}/>
-               <YAxis domain={["dataMin","dataMax"]}/>
-            </AreaChart>
-        </ResponsiveContainer>
+      <ResponsiveContainer>
+        <AreaChart data={formatData(data)}>
+          <defs>
+            <linearGradient id="chartColor" x1="0" y1="0" x2="0" y2="1">
+              <stop
+                offset="5%"
+                stopColor={darkMode ? "#312e81" : "rgb(199 210 254)"}
+                stopOpacity={0.8}
+              />
+              <stop
+                offset="95%"
+                stopColor={darkMode ? "#312e81" : "rgb(199 210 254)"}
+                stopOpacity={0}
+              />
+            </linearGradient>
+          </defs>
+          <Area
+            type="monotone"
+            dataKey="value"
+            stroke="#312e81"
+            fillOpacity={1}
+            fill="url(#chartColor)"
+            strokeWidth={0.5}
+          />
+          <Tooltip
+            contentStyle={darkMode ? { backgroundColor: "#111827" } : null}
+            itemStyle={darkMode ? { color: "#818cf8" } : null}
+          />
+          <XAxis dataKey="date" />
+          <YAxis domain={["dataMin", "dataMax"]} />
+        </AreaChart>
+      </ResponsiveContainer>
     </Card>
-  )
-}
+  );
+};
 
-export default Chart
+export default Chart;
