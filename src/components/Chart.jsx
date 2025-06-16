5fc38ea9e4ead2263827d6d424f6645a91a71292
Sega <brajicsergej@gmail.com>
2025-06-16 13:44:26 +0200

    Linear gradient chart

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -21,7 +21,13 @@ const Chart = () => {
     <Card>
         <ResponsiveContainer>
             <AreaChart data={formatData()}>
-               <Area type="monotone" dataKey="value" stroke='#312e81' fillOpacity={1} strokeWidth={0.5}/>
+        <defs>
+              <linearGradient id="chartColor" x1="0" y1="0" x2="0" y2="1">
+                 <stop offset="5%" stopColor="rgb(199 210 254)" stopOpacity={0.8}/>
+                 <stop offset="95%" stopColor="rgb(199 210 254)" stopOpacity={0}/>
+              </linearGradient>
+        </defs>
+               <Area type="monotone" dataKey="value" stroke='#312e81' fillOpacity={1} strokeWidth={0.5} fill='url(#chartColor)'/>
                <Tooltip/>
                <XAxis dataKey={"date"}/>
                <YAxis domain={["dataMin","dataMax"]}/>
