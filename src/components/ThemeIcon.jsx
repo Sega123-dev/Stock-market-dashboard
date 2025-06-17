235c48dfca2dbfc4324b8d7a8a63a4e40694d7a8
Sega <brajicsergej@gmail.com>
2025-06-17 23:18:44 +0200

    Transitions

--- src/components/ThemeIcon.jsx
+++ src/components/ThemeIcon.jsx
@@ -8,7 +8,7 @@ const ThemeIcon = () => {
     setDarkMode(!darkMode)
   }
 
-  return <button className={`rounded-lg border-1 border-neutral-400 p-2 absolute right-8 xl:right-32 shadow-lg ${darkMode ? "shadow-gray-800" : null}`} onClick={toggleDarkMode}>
+  return <button className={`rounded-lg border-1 border-neutral-400 p-2 absolute right-8 xl:right-32 shadow-lg ${darkMode ? "shadow-gray-800" : null} transition duration-300 hover:scale-125`} onClick={toggleDarkMode}>
     <MoonIcon className={`h-8 w-8 cursor-pointer stroke-1 fill-none stroke-neutral-400 ${darkMode ? "fill-yellow-400 stroke-yellow-400" : "fill-none" }`}/>
   </button>
 }
