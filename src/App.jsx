b720646cde7ad9871257e2195b6b0421fe3053dc
Sega <brajicsergej@gmail.com>
2025-06-17 14:42:52 +0200

    API helper functions

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import { useState } from 'react';
 import './App.css'
 import Dashboard from './components/dashboard'
 import ThemeContext from './context/ThemeContext';
+import StockContext from './context/StockContext';
 
 function App() {
-  const [darkMode,setDarkMode] = useState(false)
+  const [darkMode,setDarkMode] = useState(false);
+  const [stockSymbol,setStockSymbol] = useState("FB");
+
   return(
-    <ThemeContext.Provider value={{darkMode,setDarkMode}}><Dashboard/></ThemeContext.Provider>
+    <ThemeContext.Provider value={{darkMode,setDarkMode}}>
+      <StockContext.Provider value={{stockSymbol,setStockSymbol}}><Dashboard/></StockContext.Provider>
+    </ThemeContext.Provider>
   );
 }
 
