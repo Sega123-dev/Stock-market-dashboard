e2a975fcb275cc98f55da10e5317f513770d0dd9
Sega <brajicsergej@gmail.com>
2025-06-17 23:25:42 +0200

    Small style changes

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -2,9 +2,9 @@ import Card from "./Card";
 const Overview = ({ symbol,price,change,changePercent,currency }) => {
   return (
      <Card>
-        <span className="absolute left-4 top-4 text-neutral-400 text-lg xl:text-xl 2xl:text-2xl"> {symbol}</span>
+        <span className="absolute left-4 top-4 text-neutral-400 text-base"> {symbol}</span>
         <div className="w-full h-full flex items-center justify-around">
-        <span className="text-2xl xl:text-4xl 2xl:text-5xl flex items-center">
+        <span className="text-2xl xl:text-4xl 2xl:text-4xl flex items-center">
           {price}
           <span className="text-lg xl:text-xl 2xl:text-2xl text-neutral-400 m-2">
             {currency}
