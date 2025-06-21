ffa0f243a4154dc588dcc595be2e6a1725724867
Sega <brajicsergej@gmail.com>
2025-06-21 15:05:12 +0200

    CloseIcon and context for sharing isModalOpened data

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,19 @@ import ThemeContext from './context/ThemeContext';
 import StockContext from './context/StockContext';
 import ModalWrapper from './components/ModalWrapper';
 import CloseIcon from './components/CloseIcon';
+import ModalContext from './context/ModalContext';
 
 function App() {
   const [darkMode,setDarkMode] = useState(false);
   const [stockSymbol,setStockSymbol] = useState("META");
+  const [isModalOpened,setIsModalOpened] = useState(true)
 
   return(
     <ThemeContext.Provider value={{darkMode,setDarkMode}}>
       <StockContext.Provider value={{stockSymbol,setStockSymbol}}>
-        <ModalWrapper><CloseIcon/></ModalWrapper>
+        <ModalContext.Provider value={{isModalOpened,setIsModalOpened}}>
+          <ModalWrapper><CloseIcon/></ModalWrapper>
+        </ModalContext.Provider>
         <Dashboard/>
       </StockContext.Provider>
     </ThemeContext.Provider>
