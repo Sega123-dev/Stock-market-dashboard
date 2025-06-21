4b38ddbcc8468d71b5a0681123e5d97eee593b3c
Sega <brajicsergej@gmail.com>
2025-06-21 16:41:49 +0200

    Bug fixes

--- src/components/ModalOpener.jsx
+++ src/components/ModalOpener.jsx
@@ -8,7 +8,7 @@ const ModalOpener = () => {
        setIsModalOpened(!isModalOpened)
 }
   return (
-    <button style={{zIndex:"99"}} className="absolute top-0 right-8 bg-indigo-600 p-2 m-0 rounded-b-sm cursor-pointer transition duration-300 hover:opacity-90 text-white cursor pointer text-base" onClick={toggleModal}>Subscribe to our newsletter</button>
+    <button style={{zIndex:"99"}} className="absolute top-0 right-8 bg-indigo-600 p-2 m-0 rounded-b-sm cursor-pointer transition duration-300 hover:opacity-90 text-white cursor pointer text-base quicksand" onClick={toggleModal}>Subscribe to our newsletter</button>
   )
 }
 
