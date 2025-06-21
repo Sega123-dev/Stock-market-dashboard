eeb05b419647cc357b3a165fe84a70363e42de36
Sega <brajicsergej@gmail.com>
2025-06-21 17:13:19 +0200

    Bug fixes

---
+++ src/components/ModalWrapper.jsx
@@ -0,0 +1,15 @@
+import { useContext } from "react"
+import ModalContext from "../context/ModalContext"
+import ThemeContext from "../context/ThemeContext"
+
+const ModalWrapper = ({ children }) => {
+  const {isModalOpened} = useContext(ModalContext)
+  const {darkMode} = useContext(ThemeContext)
+    return (
+    <div className={`h-screen absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center ${isModalOpened ? "block" : "hidden"} ${darkMode ? "transparent-background-dark" : "transparent-background"}`}>
+      { children }
+    </div>
+  ) 
+}
+
+export default ModalWrapper
