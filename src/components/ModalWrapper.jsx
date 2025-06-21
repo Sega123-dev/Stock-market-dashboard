9036d5ffc7bf7cee139e0127a4ea1af4ce086641
Sega <brajicsergej@gmail.com>
2025-06-21 15:58:07 +0200

    Dark mode for ModalWrapper

--- src/components/ModalWrapper.jsx
+++ src/components/ModalWrapper.jsx
@@ -1,10 +1,12 @@
 import { useContext } from "react"
 import ModalContext from "../context/ModalContext"
+import ThemeContext from "../context/ThemeContext"
 
 const ModalWrapper = ({ children }) => {
   const {isModalOpened} = useContext(ModalContext)
+  const {darkMode} = useContext(ThemeContext)
     return (
-    <div className={`h-screen absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center transparent-background ${isModalOpened ? "block" : "hidden"}`}>
+    <div className={`h-screen absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center ${isModalOpened ? "block" : "hidden"} ${darkMode ? "transparent-background-dark" : "transparent-background"}`}>
       { children }
     </div>
   ) 
